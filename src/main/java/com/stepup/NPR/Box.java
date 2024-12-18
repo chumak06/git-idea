fe4fca3840acb9871d3163e8aa564abbac77bb1d
Alexandr Chumakov <chumak06@yandex.ru>
2024-12-18 14:22:50 +0400

    готова 002

--- src/main/java/com/stepup/NPR/Box.java
+++ src/main/java/com/stepup/NPR/Box.java
@@ -3,6 +3,8 @@ package com.stepup.NPR;
 public class Box {
     public static void main(String[] args)
     {
+        System.out.println("---");
         System.out.println("Новый код");
         System.out.println("Новый код");
+        System.out.println("---");
     }}
